34698e862b5782b87101695b5703c1065d515f8c
Barnakrum <krzysiu03@tlen.pl>
2024-08-06 22:11:32 +0200

    Add background worker for loading saved config and executing removing of comments

---
+++ src/background.js
@@ -0,0 +1,47 @@
+//data-testid="comment-base-item-10002"
+//data-testid="render-reactions"
+//aria-label=":thumbsup:"
+
+chrome.runtime.onInstalled.addListener(() => {
+  let parsed = null;
+  chrome.storage.sync.get(["config"]).then((result) => {
+    if (result.config) {
+      parsed = JSON.parse(result.config);
+    }
+  });
+  const findCommentsWithReaction = async () => {
+    let [tab] = await chrome.tabs.query({
+      active: true,
+      url: "https://*.atlassian.net/jira/*",
+    });
+    if (tab) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        args: [parsed, tab],
+        func: (parsed, tab) => {
+          for (let i = 0; i < parsed.length; i++) {
+            console.log(tab);
+            if (tab.url.includes("projects/" + parsed[i].name)) {
+              let comments = document.querySelectorAll(
+                `[data-testid*="comment-base-item"]`
+              );
+              comments.forEach((comment) => {
+                for (let j = 0; j < parsed[i].rules.length; j++) {
+                  let reactions = comment.querySelectorAll(
+                    `[aria-label=":${parsed[i].rules[j].emoji}:"]`
+                  );
+                  if (reactions.length > 0) {
+                    comment.style.display = "none";
+                  }
+                }
+              });
+            }
+          }
+        },
+      });
+    }
+  };
+  setInterval(() => {
+    findCommentsWithReaction();
+  }, 5000);
+});

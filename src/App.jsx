784f98e3ba42ed953a436b6b57f32ad93975cf75
Barnakrum <krzysiu03@tlen.pl>
2024-08-06 20:39:48 +0200

    Add basic ui + saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
+
+const emptyProject = { name: "projxect", rules: [] };
+const emptyRule = { name: "rule", emoji: "" };
+
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  const saveConfig = () => {
+    let value = JSON.stringify(projects);
+    chrome.storage.sync.set({ config: value }).then(() => {
+      console.log(value);
+    });
+  };
+
+  const loadConfig = () => {
+    chrome.storage.sync.get(["config"]).then((result) => {
+      console.log(result.config);
+      setProjects(JSON.parse(result.config));
+    });
+  };
+
+  useEffect(() => {
+    loadConfig();
+  }, []);
+
   return (
     <>
-      <div>x</div>
-      <div className="bg-primary text-primary-foreground">y</div>
+      <div className="min-w-64">
+        <div className="">Projects</div>
+        <div
+          onClick={() => {
+            setProjects([...projects, emptyProject]);
+          }}>
+          +
+        </div>
+        <div
+          onClick={() => {
+            saveConfig();
+          }}>
+          save
+        </div>
+        {projects.map((project) => {
+          return <div>{project.name}</div>;
+        })}
+      </div>
     </>
   );
 }
